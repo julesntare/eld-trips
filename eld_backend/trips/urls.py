9ebb65f1cf934efc234044f48d6860a54d7bfc98
julesntare <julesntare@gmail.com>
2025-03-20 11:16:23 +0200

    added get trips by id endpoint

--- eld_backend/trips/urls.py
+++ eld_backend/trips/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import TripListView, TripListCreateView, ELDLogListView
+from .views import TripIdListView, TripListView, TripListCreateView, ELDLogListView
 
 urlpatterns = [
     path('trips/', TripListCreateView.as_view(), name='trip-list-create'),
     path('trips', TripListView.as_view(), name='trips-list'),
+    path('trips/<int:id>', TripIdListView.as_view(), name='trips-id-list'),
     path('trips/<int:trip_id>/logs/', ELDLogListView.as_view(), name='log-list'),
 ]
--- eld_backend/trips/views.py
+++ eld_backend/trips/views.py
@@ -15,6 +15,13 @@ class ELDLogListView(generics.ListAPIView):
         trip_id = self.kwargs['trip_id']
         return ELDLog.objects.filter(trip_id=trip_id)
 
+class TripIdListView(generics.ListAPIView):
+    serializer_class = TripSerializer
+
+    def get_queryset(self):
+        trip_id = self.kwargs['id']
+        return Trip.objects.filter(id=trip_id)
+
 class TripListView(generics.ListAPIView):
     serializer_class = TripSerializer
 
