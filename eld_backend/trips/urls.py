a3630824de2aa7c13d00b3d76398952f4a081900
julesntare <julesntare@gmail.com>
2025-03-20 09:29:39 +0200

    added trips list endpoint

--- eld_backend/trips/urls.py
+++ eld_backend/trips/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TripListCreateView, ELDLogListView
+from .views import TripListView, TripListCreateView, ELDLogListView
 
 urlpatterns = [
     path('trips/', TripListCreateView.as_view(), name='trip-list-create'),
+    path('trips', TripListView.as_view(), name='trips-list'),
     path('trips/<int:trip_id>/logs/', ELDLogListView.as_view(), name='log-list'),
 ]
--- eld_backend/trips/views.py
+++ eld_backend/trips/views.py
@@ -14,3 +14,9 @@ class ELDLogListView(generics.ListAPIView):
     def get_queryset(self):
         trip_id = self.kwargs['trip_id']
         return ELDLog.objects.filter(trip_id=trip_id)
+
+class TripListView(generics.ListAPIView):
+    serializer_class = TripSerializer
+
+    def get_queryset(self):
+        return Trip.objects.all()
